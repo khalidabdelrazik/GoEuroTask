d772790edd23b6d3aa2c6d8665d6209852db4bbc
KBGW1550 <KBGW1550@cai-kbgw1550.rd.francetelecom.fr>
2016-06-07 21:36:22 +0200

    Adding command line options

--- src/main/java/com/goeuro/com/goeuro/Main.java
+++ src/main/java/com/goeuro/com/goeuro/Main.java
@@ -22,9 +22,42 @@ public class Main {
 	public static void main(String[] args) throws JsonSyntaxException,
 			IOException {
 		Gson gson = new Gson();
-		Country[] staff = gson.fromJson(ReadingUrlUtil.readUrl("http://api.goeuro.com/api/v2/position/suggest/en/lannion"),
-				Country[].class);
-		System.out.println(staff.length);
+		if (args.length == 1) {
+			Country[] staff = gson
+					.fromJson(
+							ReadingUrlUtil
+									.readUrl("http://api.goeuro.com/api/v2/position/suggest/en/"
+											+ args[0]), Country[].class);
+			System.out.println(staff.length);
+		} else if (args.length > 1) {
+			if ("-u".equals(args[0])) {
+				Country[] staff = gson.fromJson(
+						ReadingUrlUtil.readUrl(args[1]), Country[].class);
+				System.out.println(staff.length);
+			}
+			if ("-o".equals(args[0])) {
+				Country[] staff = gson.fromJson(
+						ReadingUrlUtil.readUrl(args[1]), Country[].class);
+				System.out.println(staff.length);
+			} else if (args.length > 2) {
+				if ("-u".equals(args[0])) {
+					Country[] staff = gson.fromJson(
+							ReadingUrlUtil.readUrl(args[1]), Country[].class);
+					System.out.println(staff.length);
+				}
+				if ("-o".equals(args[0])) {
+					Country[] staff = gson.fromJson(
+							ReadingUrlUtil.readUrl(args[1]), Country[].class);
+					System.out.println(staff.length);
+				}
+			}
+		} else {
+			System.out
+					.println("--help for help\n"
+							+ "CITYNAME add city name \n"
+							+ "-u Optional to add a new url to REST api with city name\n"
+							+ "-o Optional output file");
+		}
 
 	}
 }
