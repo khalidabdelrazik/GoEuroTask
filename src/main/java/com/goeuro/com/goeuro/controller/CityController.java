71e77cd81883941aef7bfe23dc2bf3dd22f4639a
KBGW1550 <KBGW1550@cai-kbgw1550.rd.francetelecom.fr>
2016-06-07 21:47:28 +0200

    Adding Test Structure Adding Main Controller

---
+++ src/main/java/com/goeuro/com/goeuro/controller/CityController.java
@@ -0,0 +1,8 @@
+package com.goeuro.com.goeuro.controller;
+
+public class CityController {
+
+	public boolean exprtCityData(String cityName, String apiUrl, String outputFileName) {
+		return true;
+	}
+}
---
+++ src/test/java/com/goeuro/com/goeuro/CityControllerTest.java
@@ -0,0 +1,47 @@
+package com.goeuro.com.goeuro;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.goeuro.com.goeuro.controller.CityController;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CityControllerTest.
+ */
+public class CityControllerTest {
+	
+	/** The city controller. */
+	CityController cityController = new CityController();
+
+	/**
+	 * Instantiates a new city controller test.
+	 */
+	public CityControllerTest() {
+	}
+
+	/**
+	 * Test read city name.
+	 */
+	@Test
+	public void testReadCityName() {
+		Assert.assertTrue(true);
+	}
+	
+	/**
+	 * Test read city api url.
+	 */
+	@Test
+	public void testReadCityApiURL() {
+		Assert.assertTrue(true);
+	}
+	
+	/**
+	 * Test read city api url output file.
+	 */
+	@Test
+	public void testReadCityApiURLOutputFile() {
+		Assert.assertTrue(true);
+	}
+}
